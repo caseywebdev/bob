a65d4780eab5b7527426c20fdb392f90e0d3ab34
Casey Foster <c@sey.me>
2017-07-25 13:58:21 -0500

    Fix notify

--- src/server/utils/notify.js
+++ src/server/utils/notify.js
@@ -2,5 +2,7 @@ const getDb = require('./get-db');
 
 module.exports = async ({channel, data}) => {
   const db = await getDb();
-  return db.raw('NOTIFY ?? ?', [channel, JSON.stringify(data)]);
+  return db.raw(
+    db.raw('NOTIFY ??, ?', [channel, JSON.stringify(data)]).toQuery()
+  );
 };
