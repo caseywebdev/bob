9a9b8a640f28e2c59e8771dcfdd0386fea97d2ef
Casey Foster <c@sey.me>
2017-07-26 15:35:41 -0500

    Fix dumb check

--- src/server/utils/get-build-args.js
+++ src/server/utils/get-build-args.js
@@ -6,17 +6,14 @@ module.exports = async ({
   env,
   env: {config: {docker: {buildVariables}}}
 }) => {
-  console.log('buildArgs', buildArgs);
-  console.log('buildVariables', buildVariables);
-  console.log('env', env);
-  if (!_.isEmpty(buildArgs)) return {};
+  if (_.isEmpty(buildArgs)) return {};
 
   const vars = {REF: ref, SHA: sha};
   for (let key in buildVariables) {
     const {value, vault: {path, key: vaultKey} = {}} = buildVariables[key];
     vars[key] = value || (await getVault({env}).get(path))[vaultKey];
   }
-  console.log('vars', vars);
+
   return _.mapObject(buildArgs, str =>
     _.reduce(
       vars,
