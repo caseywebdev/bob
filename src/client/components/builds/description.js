d51d005903b0ab40535226407e33cf6ca0d8cfb1
Casey Foster <c@sey.me>
2017-08-03 13:02:25 -0500

    Make it more awesome

--- src/client/components/builds/description.js
+++ src/client/components/builds/description.js
@@ -1,12 +1,13 @@
 import _ from 'underscore';
 import {Link} from 'react-router-dom';
 import {withPave} from 'pave-react';
-import buildIsDone from '../../utils/build-is-done';
-import getBuildDescription from '../../../shared/utils/get-build-description';
-import STATUS_INFO from '../../../shared/constants/status-info';
 import {WRITE} from '../../../shared/constants/permission-levels';
+import buildIsDone from '../../utils/build-is-done';
+import ErrorComponent from '../shared/error';
+import getBuildDescription from '../../../shared/utils/get-build-description';
 import history from '../../utils/history';
 import React from 'react';
+import STATUS_INFO from '../../../shared/constants/status-info';
 import styles from './description.scss';
 
 const rebuild = ({props: {build: {id}, pave: {store}}}) => {
@@ -23,20 +24,31 @@ const rebuild = ({props: {build: {id}, pave: {store}}}) => {
 const cancel = ({props: {build: {id}, pave: {store}}}) =>
   store.run({query: ['cancelBuild!', id]}).catch(::console.error);
 
-const render = ({props, props: {build, build: {error, id, status}}}) =>
+const render = ({props, props: {build, build: {error, id, status, tags}}}) =>
   <div
     className={styles.root}
     style={{borderLeftColor: STATUS_INFO[status].color}}
   >
-    <Link className={styles.link} to={`/builds/${id}`}>
-      {getBuildDescription({build, withEmoji: true})}
-    </Link>
-    {
-      build.role & WRITE > 0 && buildIsDone({build}) ?
-      <button onClick={() => rebuild({build, props})}>Rebuild</button> :
-      <button onClick={() => cancel({build, props})}>Cancel</button>
-    }
-    {error ? <div className={styles.error}>{error}</div> : null}
+    <div className={styles.left}>
+      {STATUS_INFO[status].emoji}
+    </div>
+    <div className={styles.right}>
+      <Link className={styles.link} to={`/builds/${id}`}>
+        {getBuildDescription({build})}
+      </Link>
+      {
+        build.role & WRITE > 0 && buildIsDone({build}) ?
+        <button onClick={() => rebuild({build, props})}>Rebuild</button> :
+        <button onClick={() => cancel({build, props})}>Cancel</button>
+      }
+      {_.map(tags, (tag, key) =>
+        <div {...{key}} className={styles.tag}>{tag}</div>)
+      }
+      {
+        !error ? null :
+        <div className={styles.error}><ErrorComponent {...{error}} /></div>
+      }
+    </div>
   </div>;
 
 export default withPave(props => render({props}));
--- src/client/components/shared/error.js
+++ src/client/components/shared/error.js
@@ -1,8 +1,11 @@
 import Icon from './icon';
 import React from 'react';
+import styles from './error.scss';
 
 export default ({error}) =>
-  <div>
-    <Icon name='exlamation-circle' />{' '}
-    {error ? error.message || error : 'Error'}
+  <div className={styles.root}>
+    <div className={styles.left}><Icon name='exclamation-circle' /></div>
+    <div className={styles.right}>
+      {error ? error.message || error : 'Error'}
+    </div>
   </div>;
