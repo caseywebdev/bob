aa8ba8b7907f587a1571a75848b04ada61a7e95a
Casey Foster <c@sey.me>
2017-08-02 22:54:07 -0500

    Fix reload

--- src/client/initializers/load-livereload.js
+++ src/client/initializers/load-livereload.js
@@ -5,6 +5,6 @@ const {watch} = config;
 if (watch) {
   const script = document.createElement('script');
   script.async = true;
-  script.src = '/livereload.js';
+  script.src = '/livereload.js?port=80';
   document.body.appendChild(script);
 }
