4e89ec7bf0df7b55dc8338b216e568ec251b8bae
Casey Foster <c@sey.me>
2017-08-17 20:40:07 -0500

    Handle more cases

--- src/server/utils/get-value.js
+++ src/server/utils/get-value.js
@@ -1,9 +1,9 @@
-const _ = require('underscore');
 const getConsulValue = require('./get-consul-value');
 const getVault = require('./get-vault');
 const rootVault = require('./root-vault');
 
-module.exports = async ({env, value: {consul, value, vault: {key, path}}}) =>
-  _.any(consul) ? getConsulValue(consul) :
-  key && path ? (await (env ? getVault({env}) : rootVault).get(path))[key] :
+module.exports = async ({env, value: {consul = {}, value, vault = {}}}) =>
+  consul.path ? getConsulValue(consul) :
+  vault.key && vault.path ?
+    (await (env ? getVault({env}) : rootVault).get(vault.path))[vault.key] :
   value;
