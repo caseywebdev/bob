00494aeb025b195ab7d5fca5c5dbc88a1def1bcb
Casey Foster <c@sey.me>
2017-11-03 22:06:50 -0500

    Add apollo modules

--- src/client/constants/apollo-client.js
+++ src/client/constants/apollo-client.js
@@ -1,5 +1,8 @@
-import {ApolloClient, createNetworkInterface} from 'react-apollo';
+import {ApolloClient} from 'apollo-client';
+import {HttpLink} from 'apollo-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
 
 export default new ApolloClient({
-  networkInterface: createNetworkInterface({uri: '/api/graphql'})
+  link: new HttpLink({uri: '/api/graphql'}),
+  cache: new InMemoryCache()
 });
